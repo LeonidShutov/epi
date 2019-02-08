ab80f62baa70693821c63b19689ba5347071738c
Leonid Shutov <shizopotam@gmail.com>
2019-02-08 09:16:27 +0300

    task 6.7 implemented

---
+++ arrays/T67.java
@@ -0,0 +1,36 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class T67 {
+	public static List<Integer> primes(int n) {
+		List<Integer> res = new ArrayList<>();
+		if (n < 1) {
+			return res;
+		}
+		if (n == 1) {
+			res.add(1);
+			return res;
+		}
+		res.add(2);
+		for (int i = 3; i <= n; i++) {
+			for (int j = 0; j < res.size(); j++) {
+				if (i % res.get(j) == 0) {
+					break;
+				}
+				if (j == res.size() - 1) {
+					res.add(i);
+				}
+			}
+		}
+		res.add(1);
+		return res;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(primes(18).toString());
+		System.out.println(primes(1000).toString());
+	}
+}
